11950c970c70b9be7ba2ccd481987ed70eb53edf
MUHAMMAD ALI <mc190402102@vu.edu.pk>
2023-05-22 11:13:42 +0000

    Updated model_api and added notebook

--- model_api.py
+++ model_api.py
@@ -1,22 +1,35 @@
-
 import pandas as pd
-from pycaret.classification import load_model, predict_model
 from fastapi import FastAPI
 import uvicorn
+import joblib
 
 # Create the app
 app = FastAPI()
 
 # Load trained Pipeline
-model = load_model('models/RF_Model_V1')
+# Load the saved model from file
+model = joblib.load("models/model.pkl")
 
 # Define predict function
 @app.post('/predict')
 def predict(Age, Experience, Income, Family, CCAvg, Education, Mortgage, SecuritiesAccount, CDAccount, Online, CreditCard):
-    data = pd.DataFrame([[Age, Experience, Income, Family, CCAvg, Education, Mortgage, SecuritiesAccount, CDAccount, Online, CreditCard]])
-    data.columns = ['Age', 'Experience', 'Income', 'Family', 'CCAvg', 'Education', 'Mortgage', 'SecuritiesAccount', 'CDAccount', 'Online', 'CreditCard']
-    predictions = predict_model(model, data=data) 
-    return {'prediction': list(predictions['Label'])}
+    
+    features=[{
+        'Age': Age, 
+        'Experience': Experience,
+        'Income': Income,
+        'Family': Family,
+        'CCAvg': CCAvg,
+        'Education': Education,	
+        'Mortgage': Mortgage,
+        'SecuritiesAccount': SecuritiesAccount,	
+        'CDAccount': CDAccount,	
+        'Online': Online,
+        'CreditCard': CreditCard}]
+    
+    features=pd.DataFrame.from_dict(features)
+    predictions = model.predict(features)
+    return {'prediction': predictions[0].item()}
 
 if __name__ == '__main__':
     uvicorn.run(app, host='0.0.0.0', port=8000)
